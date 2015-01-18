fd7e6ab72fc014297f921a41b6e310b8f5cfc178
jeremychone <jeremy.chone@gmail.com>
2015-01-18 09:06:09 -0800

    . updated the sql to follow BriteSnow convention (i.e. 00_... run as postgres, 01_.., 02_.., ... run as j8ql_user/j8ql_db)

--- src/test/resources/00_create-db-and-user.sql
+++ src/test/resources/00_create-db-and-user.sql
@@ -3,3 +3,6 @@ DROP DATABASE IF EXISTS j8ql_db;
 DROP USER IF EXISTS j8ql_user;
 CREATE USER j8ql_user PASSWORD 'welcome';
 CREATE DATABASE j8ql_db owner j8ql_user ENCODING = 'UTF-8';
+
+\c j8ql_db
+CREATE EXTENSION hstore;
